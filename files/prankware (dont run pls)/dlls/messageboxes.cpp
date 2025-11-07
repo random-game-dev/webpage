343a59b48d9ef07cc4799225ce36c3e88be6e39b
alkonsafe <philblakeschool@outlook.com>
2025-11-07 15:22:22 -0500

    fix cpp files so they actually work

--- files/prankware (dont run pls)/dlls/messageboxes.cpp
+++ files/prankware (dont run pls)/dlls/messageboxes.cpp
@@ -4,10 +4,16 @@ int spamtof = 1; // Set this to 1 to enable spamming
 
 void spamMessageBoxes() {
     if (spamtof == 1) {
-        for (int i = 0; i < 100; ++i) {
             MessageBoxA(NULL, "why", "why", MB_OK | MB_ICONQUESTION);
-        }
     }
 }
 
 // You can call spamMessageBoxes() from your main or other function
+
+
+// fix - alkonsafe
+int main() {
+    while (true){
+        spamMessageBoxes();
+    }
+}
